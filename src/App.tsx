87eb9285bcaab2df043f3669991f307803aaf36b
Matheus Martinez <matheus.rmartinez@gmail.com>
2021-06-13 16:23:13 -0300

    Criação Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { useState } from "react";
+
+
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseTransactionModal} ></NewTransactionModal>
       <GlobalStyle/>
     </>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,19 @@
+import Modal from 'react-modal';
+import {Container} from './styles'
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+Modal.setAppElement('#root');
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
+  return (
+    <Container>
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <h2>Cadastrar transação</h2>
+    </Modal> 
+    </Container>
+  )
+  }
