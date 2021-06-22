8231ca9d6c3001cb5c573cfcea01f4d2be1c39ab
Matheus Martinez <matheus.rmartinez@gmail.com>
2021-06-22 07:53:38 -0300

    Criação de componente botão Entrada e Saída

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from 'react-modal';
-import {Container} from './styles';
+import {Container, RadioBox, TransactionTypeContainer} from './styles';
 import closeImg from '../../assets/close.svg';
-
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { useState } from 'react';
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -10,6 +12,7 @@ interface NewTransactionModalProps {
 Modal.setAppElement('#root');
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
+  const [type, setType] = useState('deposit');
   return (
     
     <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
@@ -23,6 +26,26 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <h2>Cadastrar transação</h2>
         <input placeholder="Título"></input>
         <input placeholder="Valor" type="number"></input>
+        <TransactionTypeContainer>
+          <RadioBox
+          type="button"
+          onClick={() => setType('deposit')}
+          isActive={type === 'deposit'}
+          activeColor="green"
+          >
+          <img src={incomeImg} alt="Entrada"/>
+          <span>Entrada</span>
+          </RadioBox>
+          <RadioBox
+          type="button"
+          onClick={() => setType('withdraw')}
+          isActive={type === 'withdraw'}
+          activeColor="red"
+          >
+          <img src={outcomeImg} alt="Saída"/>
+          <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
         <input placeholder="Categoria" type="number"></input>
         <button type="submit">Cadastrar</button>
     </Container>
